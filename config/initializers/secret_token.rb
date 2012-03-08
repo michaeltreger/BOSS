ec35ee205bbab31e7165d9b3b00d85e22b008bd0
Michael Treger <michael.treger@gmail.com>
2012-03-08 15:22:07 -0800

    Added skeleton code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CS169CampusScheduler::Application.config.secret_token = '425047e32bf10c3bb03122a4c6222fc9aaf4ceea3c7b69924d0cd52b41179b59659f774e20627fb04762a98599875e001c46903019dfbe2b2a16e3f43cb13ee7'
