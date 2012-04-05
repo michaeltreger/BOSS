087187cd5a5d7c3902bed020259d4fbff2ff6728
Rohan Cribbs <rohan.cribbs@gmail.com>
2012-04-05 16:01:31 -0700

    Fix: user_type data type from stirng to integer.

--- db/migrate/20120311205056_create_users.rb
+++ db/migrate/20120311205056_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :user_type
+      t.integer :user_type
       t.string :name
       t.string :email
       t.integer :cas_user
