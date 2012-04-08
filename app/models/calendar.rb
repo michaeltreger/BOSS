5f18ee384bbabc8b290e9c53193e6a20d3b77854
Michael Treger <michael.treger@gmail.com>
2012-04-08 12:16:37 -0700

    Add: delete old entries from database when the user deletes them from their calendar

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -11,7 +11,7 @@ class Calendar < ActiveRecord::Base
     end
 
     def update_calendar(entries)
-      #old_entry_ids = self.entries.map{ |e| e.id}
+      old_entry_ids = self.entries.map{ |e| e.id}
       self.entries.clear
 
       entries.each do |ent|
@@ -26,10 +26,10 @@ class Calendar < ActiveRecord::Base
           e.update_attributes!(ent)
         end
         self.entries << e
-        #old.entry_ids.delete(e.id)
+        old_entry_ids.delete(e.id)
       end
       self.save!
-      #Entry.delete_all(old_entry_ids)
+      Entry.delete(old_entry_ids)
     end
 
 end
