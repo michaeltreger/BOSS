fac6689a2e54596f311af013539ea837620e805e
Su Yan Fang <fang@berkeley.edu>
2012-04-08 17:53:21 -0700

    nothing changed

--- app/controllers/substitutions_controller.rb
+++ app/controllers/substitutions_controller.rb
@@ -89,10 +89,8 @@ class SubstitutionsController < ApplicationController
   # DELETE /substitutions/1
   # DELETE /substitutions/1.json
   def destroy
-    @substitution = Substitution.find_by_id(params[:id])
-   # @substitution = Substitution.find(params[:id])
+    @substitution = Substitution.find(params[:id])
     @substitution.destroy
-
     respond_to do |format|
       format.html { redirect_to substitutions_url }
       format.json { head :ok }
