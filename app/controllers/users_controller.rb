1d25c5a8bef40ffce5054e483affd0194e057ba0
Peter Cheng <petercheng00@gmail.com>
2012-04-17 11:43:40 -0700

    rearranged sub tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,6 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
-        debugger
         format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ CS169CampusScheduler::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-
+  config.assets.compress = false
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile = ['*.css','*.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
