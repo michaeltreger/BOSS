ac9312458d47fd25d7b232e30e882eb877089004
Michael Treger <michael.treger@gmail.com>
2012-04-21 14:26:05 -0700

    added period to seed

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,3 +1,20 @@
 class Period < ActiveRecord::Base
   has_many :calendars, :dependent => :destroy
+
+  def create_calendars
+    # TODO transactions?
+    User.all.each do |user|
+      availability_calendar = Calendar.create!(:name=> "#{user.name} #{name} Availabilities", 
+                                  :calendar_type=>Calendar::AVAILABILITY, :user_id=>user.id, :period_id=>id)
+      shifts_calendar = Calendar.create!(:name=> "#{user.name} #{name} Shifts", 
+                                  :calendar_type=>Calendar::SHIFTS, :user_id=>user.id, :period_id=>id)
+
+      user.calendars << availability_calendar
+      user.calendars << shifts_calendar
+      user.save!
+      calendars << availability_calendar
+      calendars << shifts_calendar
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(:user_type=>0, :name=>"Michael", :cas_user=>720656, :initials=>"MT", :email=>"michael.treger@gmail.com")
 User.create(:user_type=>1, :name=>"Su Yan Fang", :cas_user =>967198)
+
+
+Period.create(:start_date=>DateTime.parse("Jan 20, 2012"), :end_date=>DateTime.parse("May 20, 2012"), :name=>"Spring 2012")
+Period.find(1).create_calendars
+Period.find(1).save!
