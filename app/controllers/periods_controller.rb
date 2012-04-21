9301c64fe8549b70d6513635d1781749a07e825d
Michael Treger <michael.treger@gmail.com>
2012-04-21 13:42:52 -0700

    Period controller

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -45,11 +45,16 @@ class PeriodsController < ApplicationController
     
     # TODO transactions?
     User.all.each do |user|
-      calendar = Calendar.create!(:name=> "#{user.name} #{@period.name}", 
+      availability_calendar = Calendar.create!(:name=> "#{user.name} #{@period.name} Availabilities", 
                                   :calendar_type=>Calendar::AVAILABILITY, :user_id=>user.id, :period_id=>@period.id)
-      user.calendars << calendar
+      shifts_calendar = Calendar.create!(:name=> "#{user.name} #{@period.name} Shifts", 
+                                  :calendar_type=>Calendar::SHIFTS, :user_id=>user.id, :period_id=>@period.id)
+
+      user.calendars << availability_calendar
+      user.calendars << shifts_calendar
       user.save!
-      @period.calendars << calendar
+      @period.calendars << availability_calendar
+      @period.calendars << shifts_calendar
     end
 
     respond_to do |format|
