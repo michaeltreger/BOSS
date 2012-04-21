3e665b4d0157f07a1d0d0f92e9ea2cd73686ccdf
Su Fang <fang@berkeley.edu>
2012-04-20 23:57:40 -0700

    temporarily create a user add it into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(:user_type=>1, :name=>"Su Yan Fang", :cas_user =>967198)
