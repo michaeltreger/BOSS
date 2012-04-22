f0dfc00560d041a7229439b8e5d97d33592368fc
Michael Treger <michael.treger@gmail.com>
2012-04-22 14:53:46 -0700

    added admin/ to lab routes

--- spec/routing/labs_routing_spec.rb
+++ spec/routing/labs_routing_spec.rb
@@ -4,31 +4,31 @@ describe LabsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/labs").should route_to("labs#index")
+      get("/admin/labs").should route_to("labs#index")
     end
 
     it "routes to #new" do
-      get("/labs/new").should route_to("labs#new")
+      get("/admin/labs/new").should route_to("labs#new")
     end
 
     it "routes to #show" do
-      get("/labs/1").should route_to("labs#show", :id => "1")
+      get("/admin/labs/1").should route_to("labs#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/labs/1/edit").should route_to("labs#edit", :id => "1")
+      get("/admin/labs/1/edit").should route_to("labs#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/labs").should route_to("labs#create")
+      post("/admin/labs").should route_to("labs#create")
     end
 
     it "routes to #update" do
-      put("/labs/1").should route_to("labs#update", :id => "1")
+      put("/admin/labs/1").should route_to("labs#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/labs/1").should route_to("labs#destroy", :id => "1")
+      delete("/admin/labs/1").should route_to("labs#destroy", :id => "1")
     end
 
   end
