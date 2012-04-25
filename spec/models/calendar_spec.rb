ed7e3df8cc325f24cf5c536d4380cdc7aa17c395
Peter Cheng <petercheng00@gmail.com>
2012-04-25 00:27:47 -0700

    finished tests for subs for iteration 4

--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Calendar do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user1 = User.create!({:user_type => 1, :name => 'anoriginalname', :approved => true, :initials => "ZZ"})
+    @user2 = User.create!({:user_type => 1, :name => 'mockuser', :approved => true, :initials => ":)"})
+    @calendar1 = Calendar.create!({:calendar_type => 1, :name => 'reallyimportant', :user_id => @user1.id})
+    @lab1 = Lab.create!({:name => 'lab1', :initials => 'l1', :max_employees => 1000, :min_employees => 1})
+    @lab2 = Lab.create!({:name => 'lab2', :initials => 'l2', :max_employees => 2000, :min_employees => 1999})
+    Entry.create!(:user_id => @user1.id, :calendar_id => @calendar1.id, :lab_id => @lab1.id, :start_time => '8:00am', :end_time => '12:00pm')
+    @overlap = Entry.create!(:user_id => @user2.id, :calendar_id => @calendar2.id, :lab_id => @lab1.id, :start_time => '9:00am', :end_time => '1:00pm' )
+    @backtobackdiff = Entry.create!(:user_id => @user2.id, :calendar_id => @calendar2.id, :lab_id => @lab2.id, :start_time => '12:00pm', :end_time => '1:00pm')
+    @backtobacksame = Entry.create!(:user_id => @user2.id, :calendar_id => @calendar2.id, :lab_id => @lab1.id, :start_time => '12:00pm', :end_time => '1:00pm')
+    @other_entry = Entry.create!(:user_id => @other.id, :calendar_id => @other_calendar.id, :start_time => '2:00pm', :end_time => '4:00pm')
+  end
+  describe "check if entry can be added to calendar" do
+    it "should disallow overlapping entries" do
+      @calendar1.canAdd(@overlap).should == false
+    end
+    it "should disallow back to back entries at different locations" do
+      @calendar1.canAdd(@backtobackdiff).should == false
+    end
+    it "should allow back to back entries at the same location" do
+      @calendar1.canAdd(@backtobacksame).should == false
+    end
+  end
 end
