55c6d0531bf8eddedc54283a74d3399d210b81e6
Su Fang <fang@berkeley.edu>
2012-04-30 15:22:50 -0700

    set up admin time_off_request

--- config/routes.rb
+++ config/routes.rb
@@ -38,9 +38,12 @@ CS169CampusScheduler::Application.routes.draw do
   match "/groups/:id" => "groups#show"
   match "/users/:id" => "users#show"
   match "/groups" => "groups#index"
-
-
-
+  
+  namespace :admin do
+    resources :time_off_requests
+  end
+ 
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
