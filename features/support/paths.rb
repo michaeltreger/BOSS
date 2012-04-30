1c7620dcf6a39f83872bc472e3d5ac870a1beecb
Su Fang <fang@berkeley.edu>
2012-04-30 03:36:55 -0700

    fix cucumber fail

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -33,6 +33,8 @@ module NavigationHelpers
       '/time_edits/new'
     when /^my time-off requests page$/
       '/time_off_requests'
+    when /^all time-off requests page$/
+      '/time_off_requests?view=view_all'
     when /^New Request$/
       '/time_off_requests/new'
     when /^the Create New Request page$/
