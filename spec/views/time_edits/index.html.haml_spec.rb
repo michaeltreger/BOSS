12e0ef17e3c453497565a341d8e8701406a79e5c
Michael Treger <michael.treger@gmail.com>
2012-05-14 22:35:06 -0700

    fixed time edit test

--- spec/views/time_edits/index.html.haml_spec.rb
+++ spec/views/time_edits/index.html.haml_spec.rb
@@ -2,17 +2,21 @@ require 'spec_helper'
 
 describe "time_edits/index" do
   before(:each) do
+    Lab.create(:name=>"lab", :initials=>"l", :max_employees=>2, :min_employees=>1)
+    User.create!(:name => "Jim", :activated => true, :initials => "J")
     assign(:time_edits, [
       stub_model(TimeEdit,
         :user_id => 1,
         :duration => 2,
-        :lab_id => 3,
+        :lab_id => 1,
+        :start_time=>Time.now,
         :comment => "Comment"
       ),
       stub_model(TimeEdit,
         :user_id => 1,
         :duration => 2,
-        :lab_id => 3,
+        :lab_id => 1,
+        :start_time=>Time.now,
         :comment => "Comment"
       )
     ])
@@ -21,11 +25,11 @@ describe "time_edits/index" do
   it "renders a list of time_edits" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => "J", :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => 2.to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 3.to_s, :count => 2
+    assert_select "tr>td", :text => "l", :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Comment".to_s, :count => 2
   end
