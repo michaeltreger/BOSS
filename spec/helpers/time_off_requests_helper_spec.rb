e960963a0b203c0fd20d65c856100f313f3867b6
Michael Treger <michael.treger@gmail.com>
2012-05-14 22:40:29 -0700

    fixed time off request rpsec test

--- spec/helpers/time_off_requests_helper_spec.rb
+++ spec/helpers/time_off_requests_helper_spec.rb
@@ -12,8 +12,10 @@ require 'spec_helper'
 # end
 describe TimeOffRequestsHelper do
   before (:each) do
-    @old = TimeOffRequest.create!(:start_time => Time.now - 5.day, :end_time => Time.now - 1.day)
-    @new = TimeOffRequest.create!(:start_time => Time.now + 5.day, :end_time => Time.now + 10.day)
+    Period.create(:start_date=>Time.now-2.months, :end_date=>Time.now+2.months, :name=>"Period", :visible=>true)
+    @user = User.create!(:name => "John", :activated => true, :initials => "J")
+    @old = TimeOffRequest.create!(:start_time => Time.now - 5.day, :end_time => Time.now - 1.day, :user_id=>@user)
+    @new = TimeOffRequest.create!(:start_time => Time.now + 5.day, :end_time => Time.now + 10.day, :user_id=>@user)
   end
   describe "recycle" do
     it "should delete passed time off requests" do
