be051333089ff725637fd330a753ef4783b9c9ee
Michael Treger <michael.treger@gmail.com>
2012-05-16 13:11:23 -0700

    Better time edit row coloring

--- app/models/time_edit.rb
+++ app/models/time_edit.rb
@@ -1,3 +1,11 @@
 class TimeEdit < ActiveRecord::Base
   validates_presence_of :user_id, :calendar_id, :start_time, :duration
+
+  def pay_period
+    pay_period = start_time.month + start_time.year * 13  
+    if start_time.day >= 15
+      pay_period += 1
+    end
+    return pay_period
+  end
 end
