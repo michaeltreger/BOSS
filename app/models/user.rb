dbeb9978852c9a74727be78b21f1c549f2269e22
Michael Treger <michael.treger@gmail.com>
2012-05-29 17:50:07 -0700

    emails with gmail acount

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,8 @@ class User < ActiveRecord::Base
 
   ADMINISTRATOR = Group.find_or_create_by_name(:name => "Administrators", :description => "All users in this group are BOS administrators.").id
   SCHEDULER = Group.find_or_create_by_name(:name => "Schedulers", :description => "All users in this group are BOS schedulers.").id
-  EMPLOYEE = Group.find_or_create_by_name(:name => "Unassigned", :description => "All users in this group by default.").id
+  EMPLOYEE = Group.find_or_create_by_name(:name => "Employee", :description => "All users in are employees.").id
+  ALL_USERS = Group.find_or_create_by_name(:name => "All Users", :description => "All users in this group by default.").id
 
 
   def make_calendars
